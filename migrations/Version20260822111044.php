22854776b7bef8e38edf2dfd878236756d4c889b
airfly626 <aiirlyme@gmail.com>
2026-09-21 00:56:05 +0800

    mySQL

--- migrations/Version20260822111044.php
+++ migrations/Version20260822111044.php
@@ -22,7 +22,7 @@ final class Version20260822111044 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE daily_spend (id INT AUTO_INCREMENT NOT NULL, spend_date DATE NOT NULL, category_id INT NOT NULL, amount NUMERIC(10, 2) NOT NULL, description NVARCHAR(255), mood NVARCHAR(10), mood_score INT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME, deleted_at DATETIME, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE daily_spend ALTER mood_score SET DEFAULT 0');
-        $this->addSql('CREATE TABLE spend_category (id INT AUTO_INCREMENT NOT NULL, category_code NVARCHAR(2) NOT NULL, category_name NVARCHAR(10) NOT NULL, delete_flag BIT NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE spend_category (id INT AUTO_INCREMENT NOT NULL, category_code NVARCHAR(2) NOT NULL, category_name NVARCHAR(10) NOT NULL, delete_flag TINYINT NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE spend_category ALTER delete_flag SET DEFAULT 0');
 
         $this->addSql("
